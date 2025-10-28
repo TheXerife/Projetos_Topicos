026ff7b5e4ff93793e21f5d16ce3306c03a460f3
TheXerife <103859044+TheXerife@users.noreply.github.com>
2025-10-28 09:53:48 -0400

    Delete projeto02_painel directory

--- projeto02_painel/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "🚀 Instalando Jetson Interactive Panel..."
-sudo apt update
-sudo apt install -y python3-pip python3-flask python3-tk
-pip install -r requirements.txt
-
-echo "✅ Dependências instaladas!"
-echo "Para iniciar, execute:  python3 app.py"
